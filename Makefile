a293a6ba280c12f24ee80ec25c8986442e434c99
eyepatchParrot <peter.vansandt@gmail.com>
2017-06-08 01:58:43 -0500

    Sort Struct

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = clang++
-CFLAGS = -ggdb -Wall -std=c++11 -march=native
+CFLAGS = -ggdb -Wall -std=c++11 -march=native -fno-omit-frame-pointer
 PFLAGS = -O3 -DNDEBUG
 
 .PHONY: Dsort Psort
@@ -9,6 +9,13 @@ Dsort: sort.cc
 
 Psort: sort.cc
 	$(CC) $(CFLAGS) $(PFLAGS) $< -o $@
+
+time: Psort
+	time ./Psort z1e6 > /dev/null
+
+perf: Psort
+	perf record -F 99 -g ./Psort z1e6 > /dev/null
+	perf report
 
 clean:
 	rm -f Dsort Psort
--- StrSet.h
+++ StrSet.h
@@ -1,3 +1,10 @@
+// We only care about performance on the largest of datasets, so start out
+// with a maximum width partition. I'll need to play around with the depth.
+// Once a partition is full, it should be put into a StrSet just for that
+// partition.
+//
+// This doesn't let me have two different values for the depth of the leaves
+// and the depth of the branches.
 // Assumes all pointers stay valid.
 //
 // Ideas :
@@ -12,19 +19,38 @@
 #include <algorithm>
 
 class StrSet {
-  std::vector<std::tuple<std::string, std::string> > valsByKey; 
+  struct KeyVal {
+    char* key;
+    int keyLen;
+    char* val;
+    int valLen;
+
+    KeyVal(char* key, int keyLen, char* val, int valLen)
+      : keyLen(keyLen), key(key), val(val), valLen(valLen) { }
+
+    bool operator<(const KeyVal& r) const {
+      const KeyVal& l = *this;
+      int minLen = std::min(l.keyLen, r.keyLen);
+      int cmp = memcmp(l.key, r.key, minLen);
+      if (0 != cmp)
+        return cmp < 0;
+      return l.keyLen < r.keyLen;
+    }
+
+  };
+  //typedef std::tuple<std::string, std::string> KeyVal;
+  std::vector<KeyVal> valsByKey; 
 
   public:
-    void put(char* key, size_t keyLen, char* val, size_t valLen) {
-      valsByKey.push_back(make_tuple(std::string(key, keyLen), std::string(val, valLen)));
+    void put(char* key, int keyLen, char* val, int valLen) {
+      valsByKey.emplace_back(key, keyLen, val, valLen);
     }
 
     void dump(char* out, off_t outSize) {
       std::sort(valsByKey.begin(), valsByKey.end());
       for (auto keyVal : valsByKey) {
-        std::string val = std::get<1>(keyVal);
-        memcpy(out, val.data(), val.size());
-        out += val.size();
+        memcpy(out, keyVal.val, keyVal.valLen);
+        out += keyVal.valLen;
       }
     }
 };
